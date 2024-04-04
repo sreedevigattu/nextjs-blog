47047d467c2aafc35b24b99a0bd5ca320e7abc7e
sreedevigattu <sreevibhor@gmail.com>
2024-04-04 18:29:19 +0530

    added a sleep

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,3 +1,6 @@
-export default function handler(req, res) {
-    res.status(200).json({ text: 'Hello' });
-  }
+export default async function handler(req, res) {
+    console.log("Before wait");
+    await new Promise(resolve => setTimeout(resolve, 5000));
+    console.log("After wait");
+    res.status(200).json({ text: 'Hello hello again' });
+}
